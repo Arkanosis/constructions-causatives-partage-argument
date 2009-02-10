039a0b2b81a80743457593b9b53b331abd4c3624
Jérémie Roquet <jroquet@arkanosis.net>
2009-02-10 08:53:43 +0000

    [COMPILATION] 	* Added '-mail' option to compile.sh to send a compiled version to every subscriber

--- compile.sh
+++ compile.sh
@@ -1,7 +1,19 @@
 #! /bin/zsh
 
+subscribers=(arkanosis@gmail.com)
+
 repeat 3; do
   pdflatex fs.tex
 done
 
-mv fs.pdf "Formalismes syntaxiques.`svn info | sed -ne 's/Revision: \([[:digit:]]\+\)/\1/p'`.pdf"
+filename="Formalismes syntaxiques.`svn info | sed -ne 's/Revision: \([[:digit:]]\+\)/\1/p'`.pdf"
+
+mv fs.pdf "$filename"
+
+if [ $# -eq 1 ] && [ "$1" = '-mail' ]; then
+    echo
+    for subscriber in $subscribers; do
+	echo "\033[32mSending an email to \033[1m$subscriber\033[0m"
+	echo 'Build' | mutt -a "$filename" -s "[BUILD] $filename" "$subscriber"
+    done
+fi
